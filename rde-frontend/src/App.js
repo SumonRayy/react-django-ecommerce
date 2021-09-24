272e0b64cb611cafdeeff72596c6c8d015cfb3da
SumonRayy <sumonrayy@gmail.com>
2021-09-24 10:31:43 +0530

    Routes added

--- rde-frontend/src/App.js
+++ rde-frontend/src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import NavBar from './components/nav-bar/NavBar';
-import Products from './components/pages/Products';
+import Products from './pages/Products';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Products />
+      <Router>
+
+        <Switch>
+
+          <Route path="/" exact>
+            <Redirect to="/product-list"/>
+          </Route>
+
+          <Route path="/product-list">
+            <NavBar />
+            <Products />
+          </Route>
+
+        </Switch>
+
+      </Router>
     </div>
   );
 }
