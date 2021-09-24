34970b13e7f5545eb4990895dc0f63b20045e895
SumonRayy <sumonrayy@gmail.com>
2021-09-24 10:32:16 +0530

    django-cors-header added

--- rde-backend/Src/rdebe/rdebe/settings.py
+++ rde-backend/Src/rdebe/rdebe/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     "api.apps.ApiConfig",
     # Rest FW :
     "rest_framework",
+    # cors :
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -127,3 +130,8 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
